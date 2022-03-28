20e32fff4eaa039e80c31f1acea0d7f1101aa73d
Vilmar Santos <nepomuceno@gmail.com>
2022-03-28 14:49:18 -0300

    Ajustes para conceitos

--- cpa/coleta-2021/eixo1d8.py
+++ cpa/coleta-2021/eixo1d8.py
@@ -4,6 +4,13 @@ import charts
 import chartsPanda
 import connectorMySql as conn
 
+class Registro:
+    campus = ''
+    titulo = ''
+    subtitulo = ''
+    conceito = 0.0
+
+
 
 def initDicts(dictInit):     
     dictInit[0] = 0
@@ -40,6 +47,62 @@ def initBarLabels(barLabels, escala):
         barLabels.append("Não concordo, nem discordo")
         barLabels.append("Concordo")
         barLabels.append("Concordo totalmente")
+
+
+def printConceitosGerais(all, filename):
+    text = ''
+    file_object  = open(filename, "w")
+    file_object.seek(0)
+
+    
+    for key in all.keys(): 
+        print(key)
+        subtitulo = ""
+        titulo = ""
+        for reg in all.get(key):            
+                
+            if (subtitulo != "" and reg.subtitulo and reg.subtitulo != subtitulo):
+                text = text + "\n" + reg.subtitulo
+            elif (titulo != "" and not reg.subtitulo and reg.titulo != titulo):
+                text = text + "\n" + reg.titulo
+            elif (subtitulo == "" and reg.subtitulo):
+                text = text + reg.subtitulo
+            elif (subtitulo == "" and not reg.subtitulo):
+                text = text + reg.titulo
+
+            subtitulo = reg.subtitulo
+            titulo = reg.titulo
+            conceitoGeral = 0
+            if (reg.conceito - int(reg.conceito)) >= 0.5:
+                conceitoGeral = int(reg.conceito+1)
+            else:
+                conceitoGeral = int(reg.conceito)
+
+            if (reg.subtitulo):
+                #print(reg.campus + " " + reg.titulo + " " +reg.subtitulo + " " + str(reg.conceito)) 
+                text = text + "$" + str(reg.conceito) + "$" + str(conceitoGeral)
+            else:
+                #print(reg.campus + " " + reg.titulo + " SEM subtilte " + str(reg.conceito))
+                text = text  + "$" + str(reg.conceito) + "$" + str(conceitoGeral)
+
+        text = text + "\n"
+
+    
+    #for nota in conceitos:
+        #print(nota)
+        #text = text + str(nota) + '$'
+        #if (nota - int(nota)) >= 0.5:
+            #text = text + str(int(nota+1)) + '$'
+        #else:
+            #text = text + str(int(nota)) + '$'
+        #tam = tam + 1
+
+        #if ((tam%(len(campiEad)+2)) == 0) :
+            #text = text + '\n'  
+
+    
+    file_object.write(text)
+    file_object.close()
 
 def generateGraphics() :
 
@@ -245,4 +308,158 @@ def generateGraphics() :
             break
             
   
-generateGraphics()
+#generateGraphics()
+
+def generateEADConcepts() :
+    allRegistros = dict()
+    for i in range(41):
+        i = i+1
+        query = """SELECT
+                        perg.titulo,
+                        perg.sub_titulo,
+                        curso1_.nome_curso,
+                        `resposta`,
+                        COUNT(resp.resposta)
+                    FROM
+                        resposta resp
+                    INNER JOIN pergunta perg ON
+                        resp.pergunta_id = perg.id
+                    INNER JOIN usuario u ON
+                        resp.respondente_id = u.id
+                    INNER JOIN respondente_curso cursos0_ ON
+                        cursos0_.respondente_id = u.id
+                    INNER JOIN curso curso1_ ON
+                        cursos0_.curso_id = curso1_.id
+                    INNER JOIN campus campus2_ ON
+                        curso1_.campus_id = campus2_.id
+                    WHERE
+                        perg.titulo LIKE '{number})%' AND (
+                            `resposta` = 0 OR `resposta` = 1 OR `resposta` = 2 OR `resposta` = 3 OR `resposta` = 4 OR `resposta` = 5
+                        ) AND  campus2_.nome_campus = 'EAD' AND (perg.tipo_pergunta = 'ESCALA_1' OR perg.tipo_pergunta = 'ESCALA_3')
+                    GROUP BY
+                        resp.`resposta`,
+                        perg.titulo,
+                        curso1_.nome_curso,
+                        perg.sub_titulo
+                    ORDER BY
+                        perg.titulo,
+                        perg.sub_titulo,
+                        curso1_.nome_curso,
+                        resp.`resposta`"""
+
+        query = query.format(number=i)
+        #print(query)
+
+        res = conn.executeAllQuery(query)
+        #print(res)
+        
+        if (len(res) > 0) :
+            for value in res:
+                print(value)
+            break
+
+    return allRegistros
+
+
+all = generateEADConcepts()      
+#printConceitosGerais(all, "conceitosEAD.txt")
+
+def generatePresencialConcepts() :
+
+    allRegistros = dict()
+    for i in range(41):
+        i = i+1
+        query = """SELECT
+                        perg.titulo,
+                        perg.sub_titulo,
+                        campus2_.nome_campus,
+                        `resposta`,
+                        COUNT(resp.resposta)
+                    FROM
+                        resposta resp
+                    INNER JOIN pergunta perg ON
+                        resp.pergunta_id = perg.id
+                    INNER JOIN usuario u ON
+                        resp.respondente_id = u.id
+                    INNER JOIN respondente_curso cursos0_ ON
+                        cursos0_.respondente_id = u.id
+                    INNER JOIN curso curso1_ ON
+                        cursos0_.curso_id = curso1_.id
+                    INNER JOIN campus campus2_ ON
+                        curso1_.campus_id = campus2_.id
+                    WHERE
+                        perg.titulo LIKE '{number})%' AND(
+                            `resposta` = 0 OR `resposta` = 1 OR `resposta` = 2 OR `resposta` = 3 OR `resposta` = 4 OR `resposta` = 5
+                        ) AND campus2_.nome_campus <> 'EAD' AND(
+                            perg.tipo_pergunta = 'ESCALA_1' OR perg.tipo_pergunta = 'ESCALA_3'
+                        )
+                    GROUP BY
+                        perg.titulo,
+                        perg.sub_titulo,
+                        campus2_.nome_campus,
+                        resp.`resposta`
+                    ORDER BY
+                        perg.titulo,
+                        perg.sub_titulo,
+                        campus2_.nome_campus,
+                        resp.`resposta`"""        
+
+        query = query.format(number=i)
+        #print(query)
+
+        res = conn.executeAllQuery(query)
+        #print(res)     
+        registros = list()
+        if (len(res) > 0) :                   
+            reg =  Registro()  
+            totalCasos = 0
+            conceito = 0.0    
+            for value in res:
+                if (reg.campus == ''):
+                    reg.campus = value[2] 
+                    reg.titulo = value[0]           
+                    reg.subtitulo = value[1]   
+
+                if (reg.campus != value[2] or reg.subtitulo != value[1]): # mudou campus
+                    trunc = int((conceito/totalCasos)*100)
+                    reg.conceito = (trunc/100)
+                    registros.append(reg)
+                    reg = Registro()
+                    reg.campus = value[2]
+                    reg.titulo = value[0]           
+                    reg.subtitulo = value[1]
+                    totalCasos = 0
+                    conceito = 0.0
+                    
+                # calcular conceito            
+                conceito = conceito + value[3]*value[4]
+                totalCasos = totalCasos + value[4]
+
+            trunc = int((conceito/totalCasos)*100)
+            reg.conceito = (trunc/100)
+            registros.append(reg)
+
+            allRegistros[i] = registros
+            
+    return allRegistros
+            
+ 
+#all = generatePresencialConcepts()
+#printConceitosGerais(all, "conceitosPres.txt")
+
+#campus = ''
+#titulo = ''
+#subtitulo = ''
+#conceito = 0.0
+
+#for key in all.keys(): 
+    #print(key)
+    #for reg in all.get(key):
+        #if (reg.subtitulo):
+            #print(reg.campus + " " + reg.titulo + " " +reg.subtitulo + " " + str(reg.conceito))       
+        #else:
+            #print(reg.campus + " " + reg.titulo + " SEM subtilte " + str(reg.conceito))      
+        
+
+
+
